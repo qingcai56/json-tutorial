ebe4f40790ad88b09dbba65e47333439c463974d
qingcai56 <45683626+qingcai56@users.noreply.github.com>
2021-03-11 14:19:01 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -2,10 +2,12 @@
 #define LEPTJSON_H__
 //多个文件或目录结构，可以用 项目名称_目录_文件名称_H_ 这种命名方式
 
+//json的六种数据类型
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 //c没有命名空间，所以一般项目简写作为标识符的前缀
 //每个结点使用lept_value结构体表示
+//用结构体来代替命名空间
 typedef struct {
     lept_type type;
 }lept_value;
@@ -17,9 +19,10 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
-
+//传入字符串和树根节点
 int lept_parse(lept_value* v, const char* json);
 
+//获取类型的结果函数
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
